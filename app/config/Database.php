24624e81fe7090a134651c6ddf7d71ff46c5cd9f
ezzoddin <mobina_ezzoddin@semnan.ac.ir>
2021-01-03 23:41:16 +0330

    complte

--- app/config/Database.php
+++ app/config/Database.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace App\Config;
+
 use \PDO;
 use \PDOException;
 
@@ -20,6 +22,8 @@ class Database
         try {
             $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec('SET NAMES utf8');
+
         } catch (PDOException $e) {
             echo 'Connection Error: ' . $e->getMessage();
         }
